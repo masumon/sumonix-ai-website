bbad44a4ce5e187975052a9efddd4d6ccc6138d2
MUMAIN AHMED <37773524+masumon@users.noreply.github.com>
2026-03-06 06:01:32 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "sumonix-os-v1";
+const assetsToCache = [
+  "/",
+  "/index.html",
+  "/dashboard.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
